23f20af70d171cbb9a5bd7bf312526e4d516fb22
ksr4innovation <kollu.srinu@gmail.com>
2018-09-20 08:09:16 +0530

    new project created

---
+++ src/main/java/com/snkit/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,35 @@
+package com.snkit.springbootdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootApplication
+public class SpringbootdemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	UserRepository userRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("    From Command line runner  ::::::::::::::::::::::::   ");
+		
+		userRepository.findAll().forEach(user -> {
+			System.out.println("    User    "+user.toString());
+		});
+		
+	}
+	
+	
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
+}
